dd120159ac163799ca8828b0fcc4649e9b0beb8c
NathanXMills <nathan.x.mills@gmail.com>
2021-04-03 11:53:21 -0700

    part 6

--- app/src/main/java/com/example/myscouting2021training/MatchInputActivity.kt
+++ app/src/main/java/com/example/myscouting2021training/MatchInputActivity.kt
@@ -3,14 +3,28 @@ package com.example.myscouting2021training
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.EditText
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.match_input_activity.*
+
+lateinit var intent:Intent
+public var match_tag = "Match"
 
 class MatchInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.match_input_activity)
+    }
+    
+    fun startNextActivity(view: View){
+        intent = Intent( this, ScoutingActivity::class.java)
+        if(checkEditText(et_match_number, et_team_number)) {
+            intent.putExtra(match_tag, Gson().toJson(Match(getTeamNumber(), getMatchNumber())))
+            Log.e("match_data","${Gson().fromJson(intent.extras!!.get(match_tag).toString(), Match::class.java)}")
+            startActivity(intent)
+        }
     }
 
     fun getTeamNumber(): String{
@@ -19,12 +33,6 @@ class MatchInputActivity : AppCompatActivity() {
 
     fun getMatchNumber(): String{
         return et_match_number.text.toString()
-    }
-    
-    fun startNextActivity(view: View){
-        if(checkEditText(et_match_number, et_team_number)) {
-            startActivity(Intent( this, ScoutingActivity::class.java))
-        }
     }
 
     fun checkEditText(vararg et: EditText): Boolean {
--- app/src/main/java/com/example/myscouting2021training/ScoutingActivity.kt
+++ app/src/main/java/com/example/myscouting2021training/ScoutingActivity.kt
@@ -1,6 +1,28 @@
 package com.example.myscouting2021training
 
 import android.app.Activity
+import android.os.Bundle
+import android.util.Log
+import com.google.gson.Gson
+import kotlinx.android.synthetic.main.scouting_activity.*
 
 class ScoutingActivity : Activity() {
+
+    lateinit var match: Match
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.scouting_activity)
+        retrieveMatchData()
+        initScoutingSetup()
+    }
+
+    private fun retrieveMatchData(){
+        match = Gson().fromJson(intent.extras!!.get(match_tag).toString(), Match::class.java)
+    }
+
+    private fun initScoutingSetup(){
+        tv_match_number.text = match.matchNumber
+        tv_team_number.text = match.teamNumber
+    }
 }

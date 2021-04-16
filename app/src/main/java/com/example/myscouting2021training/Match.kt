b81355cc0ff231494d60f3352ccdf62d17f69d97
NathanXMills <nathan.x.mills@gmail.com>
2021-04-15 20:05:08 -0700

    complete Part 8

--- app/src/main/java/com/example/myscouting2021training/Match.kt
+++ app/src/main/java/com/example/myscouting2021training/Match.kt
@@ -2,5 +2,7 @@ package com.example.myscouting2021training
 
 data class Match (
     val teamNumber: String,
-    val matchNumber: String
+    val matchNumber: String,
+    var elementOneCount: Int = 0,
+    var elementTwoCount: Int = 0
 )
--- app/src/main/java/com/example/myscouting2021training/ScoutingActivity.kt
+++ app/src/main/java/com/example/myscouting2021training/ScoutingActivity.kt
@@ -2,12 +2,13 @@ package com.example.myscouting2021training
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.Button
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.scouting_activity.*
 
-class ScoutingActivity : Activity() {
 
+class ScoutingActivity : Activity() {
     lateinit var match: Match
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,12 +18,48 @@ class ScoutingActivity : Activity() {
         initScoutingSetup()
     }
 
-    private fun retrieveMatchData(){
+    private fun retrieveMatchData() {
         match = Gson().fromJson(intent.extras!!.get(match_tag).toString(), Match::class.java)
     }
 
-    private fun initScoutingSetup(){
+    private fun initScoutingSetup() {
         tv_match_number.text = match.matchNumber
         tv_team_number.text = match.teamNumber
+
+        btn_placement_one.text = getString(R.string.btn_placement_one, match.elementOneCount.toString())
+        btn_placement_two.text = getString(R.string.btn_placement_two, match.elementTwoCount.toString())
+
+        btn_placement_one.setOnLongClickListener{
+            //this listener will follow the same formate as the PlacementOnClick functions bellow.
+            match.elementOneCount--
+            updateButtonLabel(btn_placement_one, R.string.btn_placement_one, match.elementOneCount)
+            //LongClickListeners are not void functions, meaning they need a boolean at the end to set off their availability
+            return@setOnLongClickListener true
+        }
+        btn_placement_two.setOnLongClickListener{
+            match.elementTwoCount--
+            updateButtonLabel(btn_placement_two, R.string.btn_placement_two, match.elementTwoCount)
+            return@setOnLongClickListener true
+        }
+    }
+
+    public fun placementOneOnClick(view: View) {
+        match.elementOneCount++
+        updateButtonLabel(btn_placement_one, R.string.btn_placement_one, match.elementOneCount)
+    }
+
+    public fun placementTwoOnClick(view: View) {
+        match.elementTwoCount++
+        updateButtonLabel(btn_placement_two, R.string.btn_placement_two, match.elementTwoCount)
+    }
+
+    public fun incapOnClick(view: View) {
+    }
+
+    public fun submitOnClick(view: View) {
+    }
+
+    public fun updateButtonLabel(tv: Button, resource: Int, value: Int) {
+        tv.text = getString(resource, value)
     }
 }
